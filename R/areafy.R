20d4174368c2b306729fea3ca6fbfe78f8ba7737
matthew gregory <matthew.gregory@teachfirst.org.uk>
2016-10-07 19:38:23 +0100

    Generalised areafy() to all the ele, next sub-ele required

--- R/areafy.R
+++ R/areafy.R
@@ -15,16 +15,33 @@
 #' This should be used to provide the initial estimates of the unit_area
 #' of each element-sub-element construction type in the dataframe.
 #' It will also be called after deterioration and different grades of the elementid appear.
+#' As most methods are to use the gifa, we do this first and then specify the rarer \code{unit_area}
+#' calculations or estimation methods. A window or door is assumed to have an area of one 
+#' square metre.
 #' @seealso \link{/data-raw/blockbuster_unit_quantity_methods.csv}
 #' @export
 #' @examples
 
 areafy <- function(blockbuster_initial_state) {
   areafyed <- blockbuster_initial_state %>%
-    mutate(unit_area = if_else(element == "Roofs",
-                          true = gifa,
-                          false = 0)  #  sum(y$unit_area != 0) == sum(y$element == "Roofs")
-    )
+    mutate(unit_area = if_else(element == "Roofs", true = ground_gifa,
+                          false = if_else(element == "Floors and Stairs" |              element == "Ceilings" |
+                                            element == "Internal Walls and Doors" |     element == "Sanitary Services" |
+                                            element == "Sanitary Services" |            element == "Mechanical Services" |
+                                            element == "Electrical Services" |          element == "Redecorations" |
+                                            element == "Fixed Furniture and Fittings" | element == "External Areas",
+                                          true = gifa,
+                                          false = if_else(element == "External Walls" | element == "Windows and Doors",
+                                                          true = (block_perimeter*height) - windows_doors,
+                                                          false = if_else(element == "Playing Fields",
+                                                                          true = field_area,
+                                                                          false = 0
+                                                                            )
+                                                            )
+                                          )
+                          )
+    ) #%>%  #  Keep it readable, that's using element level, some we need sub-element detail for method
+    
   invisible(as_tibble(areafyed))
 }
 
