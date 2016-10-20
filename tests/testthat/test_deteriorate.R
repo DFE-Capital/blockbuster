840ce7a22bb59a5a79db7c018c1b1ce91fcb3570
matthew gregory <matthew.gregory@teachfirst.org.uk>
2016-10-20 16:50:33 +0100

    This is a combination of 3 commits. Identified missing mc and blockbuster function with tests complete

--- tests/testthat/test_deteriorate.R
+++ tests/testthat/test_deteriorate.R
@@ -1,10 +1,11 @@
 library(blockbuster)
-context("det_eriorate blockbuster tibble through one timestep")
+context("det_eriorate blockbuster PDS tibble through one timestep")
 
 
 
 # INPUT to test -----------------------------------------------------------
 
+v <- blockbuster_pds[1:5, ]
 w <- head(dplyr::filter(blockbuster_pds, const_type == "No mechanical ventilation or air conditioning"),
           n = 1L)  #  no det rates for this
 x <- blockbuster_pds[255, ]
@@ -24,7 +25,7 @@ test_that("det_what_tm finds the correct markov chain", {
   expect_equal(det_what_tm(z),  blockbuster_mc_list@markovchains[[97]])
 })
 
-test_that("deteriorate outputs two rows if appropriate", {
+test_that("det_eriorate outputs two rows if appropriate", {
   #expect_equal(nrow(det_eriorate(w)), 2)
   #expect_equal(nrow(det_eriorate(x)), 2)
   expect_equal(nrow(det_eriorate(y)), 2)
@@ -32,8 +33,32 @@ test_that("deteriorate outputs two rows if appropriate", {
   expect_equal(nrow(det_eriorate(z)), 2)
 })
 
-test_that("deteriorate outputs correct variables", {
+test_that("det_eriorate outputs correct variables", {
   expect_equal(ncol(y), ncol(det_eriorate(y)))
   expect_equal(y$unit_area, sum(det_eriorate(y)$unit_area))
   expect_equal(y$timestep + 1, head(det_eriorate(y)$timestep, n = 1L))
+})
+
+test_that("det_eriorate outputs correct grades", {
+  expect_equal(as.character(head(det_eriorate(y)$grade, 1)), "C")
+  expect_equal(as.character(tail(det_eriorate(y)$grade, 1)), "D")
+  expect_equal(as.character(head(det_eriorate(ye)$grade, 1)), "E")
+  expect_equal(as.character(tail(det_eriorate(ye)$grade, 1)), "E")
+})
+
+test_that("blockbust() produces correct row number", {
+  expect_equal(nrow(blockbust(v)), nrow(v)*2)  #  if grade not E, then row number doubles
+  expect_equal(nrow(blockbust(blockbust(v))), nrow(v)*4)  #  through 2 timesteps of deterioration
+  expect_equal(blockbust(y), det_eriorate(y))  #  identical for one row
+})
+
+test_that("blockbuster produces correct row number by list", {
+  expect_equal(blockbuster(v, forecast_horizon = 1)[[2]], 
+               dplyr::mutate(blockbust(v), cost = 0, cost_sum = 0))  #  one year, extract second tibble from list
+  expect_equal(blockbuster(v, forecast_horizon = 2)[[3]],
+               tibble::as_tibble(aggregate(unit_area ~.,
+                                           data = dplyr::mutate(blockbust(blockbust(v)), 
+                                                                cost = 0, cost_sum = 0),
+                                           FUN = sum)))  #  demonstrate blockbuster aggregateness
+  expect_equivalent(NROW(blockbuster(y, forecast_horizon = 5)[[6]]), 3)  #  One elementid starts at grade C, so C, D, E
 })
