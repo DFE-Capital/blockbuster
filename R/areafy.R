37101fa7638b1a3b99e991a944670dee47336818
matthew gregory <matthew.gregory@teachfirst.org.uk>
2017-02-25 01:20:42 +0000

    finished areafy2, all unit area methods ok except one

--- R/areafy.R
+++ R/areafy.R
@@ -118,12 +118,12 @@ areafy <- function(blockbuster_initial_state) {
 #' pds_data == blockbuster_pds[1, ]$unit_area  #  The unit_area column was added to the blockbuster_pds using this function at time zero.
 
 areafy2 <- function(blockbuster_initial_state, unit_area_methods = "PDS") {
-  # This function is an improvement on the original areafy which was found to contain some errors
-  # This areafy2 will use the dplyr case_when approach to improve ease of reading
+  # This function is an improvement on the original areafy which was found to contain some errors.
+  # This areafy2 will use the dplyr case_when approach to improve ease of reading.
   # We should go from the most specific (element, sub_element, const_type) required to assign
   # unit_area calculation method, to the most general (just element required to assign)
   # case_when is still experimental so we may prefer to keep areafy() and fix.
-  
+  # The unit_area methods are based on expert opinion used to help quantify cost of repairs in PDS buildings.
   
   # INPUT CHECKS ------------------------------------------------------------
   if (unit_area_methods != "PDS") stop("This function currently only supports unit area estimation methods
@@ -151,31 +151,72 @@ areafy2 <- function(blockbuster_initial_state, unit_area_methods = "PDS") {
   variables_to_check <- c("gifa", "ground_gifa", "block_perimeter",
                           "block_perimeter", "height", "windows_doors",
                           "site_area_exc_field", "boundary_length",
-                          "field_area", "swimming_pool", "composition")
+                          "field_area", "swimming_pool", "composition",
+                          "number_lifts")
   lapply(variables_to_check, f)
 
 
+# CALL DPLYR --------------------------------------------------------------
+require(dplyr)  
+  
 # INSPECT BUILDING COMPONENT THEN CALCULATE UNIT AREA ---------------------------------------------------------------
 
   areafyed <- blockbuster_initial_state %>%
-    dplyr::mutate(unit_area = case_when(
+    dplyr::mutate(unit_area = dplyr::case_when(
+      
+      # External Areas and five distinctions
+      .$sub_element == "Boundary walls and fences" ~ .$boundary_length,
+      .$sub_element == "Other walls, fences and barriers, including around tennis courts, MUGAs etc" ~ .$site_area_exc_field - .$ground_gifa,
+      .$sub_element == "Swimming Pools - Plant" | .$sub_element == "Swimming Pools - Structure" ~ as.double(.$swimming_pool),
+      .$sub_element == "Drainage - Other" | .$sub_element == "Drainage - Treatment plant" ~ .$gifa ,
+      .$element == "External Areas" ~ .$site_area_exc_field - .$ground_gifa,
+        
+      # Floors and Stairs, three distinctions
       
       #  match on alpha numeric on strings with dodgy punctuation e.g. weird hyphen en-dash Alt + 0150
       #  This does exist but it's not being detected due to different encodings?
       iconv(.$sub_element, from = "UTF-8", to = "ASCII", sub = "byte") ==
         paste0("Suspended floors ", "<96>", " Structure") ~ .$gifa - .$ground_gifa,
       
+      #  this incorporates the suspended floors (matching fails) and ground / bearing hollow floors
+      .$element == "Floors and Stairs" & .$const_type == "Generally" ~ .$ground_gifa,
+      
+      #  all other floors and stairs
+      .$element == "Floors and Stairs" ~ .$gifa,
+      
+      #  Windows and doors two distinctions
+      .$element == "External Walls, Windows and Doors" & .$sub_element == "Windows and doors" ~ .$windows_doors,
+      .$element == "External Walls, Windows and Doors" ~ (.$block_perimeter * .$height) - .$windows_doors,
+      
+      #  Electrical Services have two distinctions
+      .$sub_element == "Lifts" ~ .$number_lifts, 
+      .$element == "Electrical Services" ~ .$gifa,
+      
       #  Roofs are all estimated as being ground floor gifa
       .$element == "Roofs" ~ .$ground_gifa,
       
       .$element == "Playing Fields" ~ .$field_area,
       
+      .$element == "Redecorations" ~ .$gifa, 
       
-      TRUE ~ 0
+      .$element == "Fixed Furniture and Fittings" ~ .$gifa,
+      
+      .$element == "Ceilings" ~ .$gifa,
+      
+      .$element == "Internal Walls and Doors" ~ .$gifa,
+      
+      .$element == "Sanitary Services" ~ .$gifa,
+      
+      .$element == "Mechanical Services" ~ .$gifa,
+      
+      TRUE ~ 0  #  if no method default to zero
       )
       )
   
-  
+
+# TESTER ------------------------------------------------------------------
+#  sum(areafy2(y)$unit_area == 0)
+#  filter(areafy2(y), element == "Fixed Furniture and Fittings")[, c("unit_area", "gifa", "composition")]
 
 # CONSIDER COMPOSITION ----------------------------------------------------
   areafyed <- areafyed %>%
